75fddcb348e4790f073251c26809439d35891ace
riftscript <sheikhriffat299@gmail.com>
2026-08-01 01:50:30 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/emotion-quiz",
+  assetPrefix: "/emotion-quiz/",
   experimental: {
     useWasmBinary: true,
   },
